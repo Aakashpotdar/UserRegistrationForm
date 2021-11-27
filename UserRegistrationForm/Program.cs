bcbfebbc4bfd418eed3ad60e0568a42fd92c5436
aakashpotdar <potdaraakash@gmail.com>
2021-11-27 17:08:31 +0530

    Add project files.

---
+++ UserRegistrationForm/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+
+namespace UserRegistrationForm
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--------*Registration form*--------");
+        uc1: Console.WriteLine("First Name(First letter shuld be capitle)");
+            string FistName = Console.ReadLine();
+            if (!isValid.CheckFirstName(FistName))
+            {
+                Console.WriteLine("enter valid name");
+                goto uc1;
+            }
+        uc2: Console.WriteLine("First Name(First letter shuld be capitle)");
+            string LastName = Console.ReadLine();
+            if (!isValid.CheckLastName(LastName))
+            {
+                Console.WriteLine("enter valid last name");
+                goto uc2;
+            }
+        uc3: Console.WriteLine("Enter the Mail Id");
+            string MailId = Console.ReadLine();
+            if (!isValid.CheckMailId(MailId))
+            {
+                Console.WriteLine("Mail Id is invalid");
+                goto uc3;
+            }
+        uc4: Console.WriteLine("Enter the Mobil number(include +91-)");
+            string MobileNumber = Console.ReadLine();
+            if (!isValid.CheckMobileNumber(MobileNumber))
+            {
+                Console.WriteLine("Mobile number is invalid");
+                goto uc4;
+            }
+        uc5: Console.WriteLine("Enter the Password(enter min 8 charector)");
+            string Password = Console.ReadLine();
+            if (!isValid.CheckPassword(Password))
+            {
+                Console.WriteLine(" invalid password");
+                goto uc5;
+            }
+        uc6: Console.WriteLine("Enter the Password(enter min 8 charector and atlist 1 Uppercase latter)");
+            string Password1 = Console.ReadLine();
+            if (!isValid.CheckPassword1(Password1))
+            {
+                Console.WriteLine(" invalid password");
+                goto uc6;
+            }
+        uc7: Console.WriteLine("Enter the Password(enter min 8 charector and atlist 1 Uppercase and atlist 1 number)");
+            string Password2 = Console.ReadLine();
+            if (!isValid.CheckPassword2(Password2))
+            {
+                Console.WriteLine(" invalid password");
+                goto uc7;
+            }
+        uc8: Console.WriteLine("Enter the Password(enter min 8 charector and atlist 1 Uppercase and atlist 1 number and 1 special char)");
+            string Password3 = Console.ReadLine();
+            if (!isValid.CheckPassword3(Password3))
+            {
+                Console.WriteLine(" invalid password");
+                goto uc8;
+            }
+        }
+    }
+}
---
+++ UserRegistrationForm/isValid.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace UserRegistrationForm
+{
+    class isValid
+    {
+        public static bool CheckFirstName(string name)
+        {
+            string regexName = "^([A-Z][a-z]{3}[a-z]+)$";
+            return Regex.IsMatch(name, regexName);
+        }
+        public static bool CheckLastName(string name)
+        {
+            string regexLastName = "^([A-Z][a-z]{3}[a-z]+)$";
+            return Regex.IsMatch(name, regexLastName);
+        }
+        public static bool CheckMailId(string Id)
+        {
+            string regexId = "^([a-zA-Z0-9]+)(.?)([a-zA-Z0-9]+)([@][a-z]+)([.][a-z]+)$";
+            return Regex.IsMatch(Id, regexId);
+        }
+        public static bool CheckMobileNumber(string Number)
+        {
+            string regexNumber = "^[+]{1}[9][1][-][6-9][0-9]{9}$";
+            return Regex.IsMatch(Number, regexNumber);
+        }
+        public static bool CheckPassword(string Password)
+        {
+            string regexPassword = "^([a-zA-Z]{8})([a-zA-Z]+)?$";
+            return Regex.IsMatch(Password, regexPassword);
+        }
+        public static bool CheckPassword1(string Password1)
+        {
+            string regexPassword1 = "^(?=.*[A-Z])[A-Za-z]{8,}$";
+            return Regex.IsMatch(Password1, regexPassword1);
+        }
+
+        public static bool CheckPassword2(string Password2)
+        {
+            string regexPassword2 = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$";
+            return Regex.IsMatch(Password2, regexPassword2);
+        }
+        public static bool CheckPassword3(string Password3)
+        {
+            string regexPassword3 = "^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[@#$^&*])(?=.*?[0-9]).{8,}$";
+            return Regex.IsMatch(Password3, regexPassword3);
+        }
+    }
+}
